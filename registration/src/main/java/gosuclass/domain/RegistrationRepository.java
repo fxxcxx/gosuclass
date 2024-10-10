5d4f8489de58c45878e9a6a3e6772280543bfebf
fxxcxx <rkdrornf99@naver.com>
2024-10-10 20:15:08 +0900

    Feat : registrationAPI

--- registration/src/main/java/gosuclass/domain/RegistrationRepository.java
+++ registration/src/main/java/gosuclass/domain/RegistrationRepository.java
@@ -1,5 +1,5 @@
 package gosuclass.domain;
-
+import java.util.List;  // java.util 패키지에서 List를 import
 import gosuclass.domain.*;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -9,5 +9,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
     collectionResourceRel = "registrations",
     path = "registrations"
 )
-public interface RegistrationRepository
-    extends PagingAndSortingRepository<Registration, Long> {}
+public interface RegistrationRepository extends PagingAndSortingRepository<Registration, Long> {
+
+    // classId로 Registration 엔티티들을 조회하는 메서드
+    List<Registration> findByClassId(Long classId);
+}
